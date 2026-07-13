60db36960f275e587d715a2569d87d55aa7bea33
PerspicaciousGuy <joy.0839b@gmail.com>
2026-07-13 20:44:22 +0530

    feat: integrate GlobalSettingsMixin into Database class for enhanced global settings management

--- database/users_chats_db.py
+++ database/users_chats_db.py
@@ -1,17 +1,25 @@
 import motor.motor_asyncio
 
 from info import DATABASE_NAME, USER_DB_URI
+from database.global_settings_db import GlobalSettingsMixin
 from database.users_chats_premium_db import PremiumUsageMixin
 from database.users_chats_settings_db import ChatSettingsMixin
 from database.users_chats_user_db import UserRecordsMixin
 
 
-class Database(PremiumUsageMixin, UserRecordsMixin, ChatSettingsMixin):
+class Database(
+    PremiumUsageMixin,
+    UserRecordsMixin,
+    ChatSettingsMixin,
+    GlobalSettingsMixin,
+):
     def __init__(self, uri, database_name):
         self._client = motor.motor_asyncio.AsyncIOMotorClient(uri)
         self.db = self._client[database_name]
         self.col = self.db.users
         self.grp = self.db.groups
         self.users = self.db.uersz
+        self.global_settings = self.db.global_settings
+        self.global_settings_audit = self.db.global_settings_audit
 
 db = Database(USER_DB_URI, DATABASE_NAME)
