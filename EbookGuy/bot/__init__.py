4e7a47fba54ed6cf0d5111be83736f5db2a88680
PerspicaciousGuy <joy.0839b@gmail.com>
2026-07-13 07:03:48 +0530

    refactor: simplify message iteration by using MessageRange dataclass

--- EbookGuy/bot/__init__.py
+++ EbookGuy/bot/__init__.py
@@ -1,10 +1,17 @@
-
+from dataclasses import dataclass
+from typing import AsyncGenerator
 
 from pyrogram import Client
 from pyrogram.types import Message
-from info import *
+from info import API_HASH, API_ID, BOT_TOKEN, SESSION
 from utils import temp
-from typing import Union, Optional, AsyncGenerator
+
+
+@dataclass(frozen=True)
+class MessageRange:
+    chat_id: int | str
+    limit: int
+    offset: int = 0
 
 
 class EbookGuyXBot(Client):
@@ -25,39 +32,16 @@ class EbookGuyXBot(Client):
     
     async def iter_messages(
         self,
-        chat_id: Union[int, str],
-        limit: int,
-        offset: int = 0,
-    ) -> Optional[AsyncGenerator[Message, None]]:
-        """Iterate through a chat sequentially.
-        This convenience method does the same as repeatedly calling :meth:`~pyrogram.Client.get_messages` in a loop, thus saving
-        you from the hassle of setting up boilerplate code. It is useful for getting the whole chat messages with a
-        single call.
-        Parameters:
-            chat_id (``int`` | ``str``):
-                Unique identifier (int) or username (str) of the target chat.
-                For your personal cloud (Saved Messages) you can simply use "me" or "self".
-                For a contact that exists in your Telegram address book you can use his phone number (str).
-                
-            limit (``int``):
-                Identifier of the last message to be returned.
-                
-            offset (``int``, *optional*):
-                Identifier of the first message to be returned.
-                Defaults to 0.
-        Returns:
-            ``Generator``: A generator yielding :obj:`~pyrogram.types.Message` objects.
-        Example:
-            .. code-block:: python
-                for message in app.iter_messages("pyrogram", 1, 15000):
-                    print(message.text)
-        """
-        current = offset
+        request: MessageRange,
+    ) -> AsyncGenerator[Message, None]:
+        """Yield a bounded range of messages in ascending batches."""
+        current = request.offset
         while True:
-            new_diff = min(200, limit - current)
+            new_diff = min(200, request.limit - current)
             if new_diff <= 0:
                 return
-            messages = await self.get_messages(chat_id, list(range(current, current+new_diff+1)))
+            message_ids = list(range(current, current + new_diff + 1))
+            messages = await self.get_messages(request.chat_id, message_ids)
             for message in messages:
                 yield message
                 current += 1
