599659feb266563e77b6c421c7a278f8c8cedcbc
PerspicaciousGuy <joy.0839b@gmail.com>
2026-07-13 07:03:58 +0530

    refactor: streamline file deletion logic and enhance database operations in commands

--- EbookGuy/features/admin/commands.py
+++ EbookGuy/features/admin/commands.py
@@ -1,13 +1,13 @@
-import asyncio, logging, os, re, sys
+import asyncio, logging, os, sys
 
 from pyrogram.errors import RPCError
 from pymongo.errors import PyMongoError
 
 from pyrogram import enums
-from pyrogram.types import *
-from database.ia_filterdb import col, sec_col, unpack_new_file_id, get_bad_files
+from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
+from database.ia_filterdb import col, delete_file_record, sec_col, unpack_new_file_id, get_bad_files
 from database.users_chats_db import db
-from info import *
+from info import CHANNELS
 from utils import get_size
 
 logger = logging.getLogger(__name__)
@@ -56,50 +56,15 @@ async def handle_delete(bot, message):
         return
     
     file_id = unpack_new_file_id(media.file_id)
-
-    result = col.delete_one({
-        'file_id': file_id,
-    })
-    if not result.deleted_count:
-        result = sec_col.delete_one({
-            'file_id': file_id,
-        })
-    if result.deleted_count:
+    deleted_count = await delete_file_record(
+        file_id,
+        media.file_name,
+        media.file_size,
+    )
+    if deleted_count:
         await msg.edit('File is successfully deleted from database')
     else:
-        file_name = re.sub(r"(_|\-|\.|\+)", " ", str(media.file_name))
-        unwanted_chars = ['[', ']', '(', ')']
-        for char in unwanted_chars:
-            file_name = file_name.replace(char, '')
-        file_name = ' '.join(filter(lambda x: not x.startswith('@'), file_name.split()))
-    
-        result = col.delete_many({
-            'file_name': file_name,
-            'file_size': media.file_size
-        })
-        if not result.deleted_count:
-            result = sec_col.delete_many({
-                'file_name': file_name,
-                'file_size': media.file_size
-            })
-        if result.deleted_count:
-            await msg.edit('File is successfully deleted from database')
-        else:
-            # files indexed before https://github.com/EvamariaTG/EvaMaria/commit/f3d2a1bcb155faf44178e5d7a685a1b533e714bf#diff-86b613edf1748372103e94cacff3b578b36b698ef9c16817bb98fe9ef22fb669R39 
-            # have original file name.
-            result = col.delete_many({
-                'file_name': media.file_name,
-                'file_size': media.file_size
-            })
-            if not result.deleted_count:
-                result = sec_col.delete_many({
-                    'file_name': media.file_name,
-                    'file_size': media.file_size
-                })
-            if result.deleted_count:
-                await msg.edit('File is successfully deleted from database')
-            else:
-                await msg.edit('File not found in database')
+        await msg.edit('File not found in database')
 
 async def handle_delete_all_index(bot, message):
     await message.reply_text(
@@ -115,8 +80,8 @@ async def handle_delete_all_index(bot, message):
     )
 
 async def handle_delete_all_index_confirm(bot, query):
-    col.drop()
-    sec_col.drop()
+    await col.drop()
+    await sec_col.drop()
     await query.answer('Piracy Is Crime')
     await query.message.edit('Succesfully Deleted All The Indexed Files.')
 
@@ -174,10 +139,10 @@ async def handle_stats(client, message):
         total_banned = await db.get_banned_users_count()
 
         # Files
-        total_files = col.count_documents({}) + sec_col.count_documents({})
+        total_files = await col.count_documents({}) + await sec_col.count_documents({})
         size_pipeline = [{'$group': {'_id': None, 'total': {'$sum': '$file_size'}}}]
-        size_result = [x for x in col.aggregate(size_pipeline)]
-        sec_size_result = [x for x in sec_col.aggregate(size_pipeline)]
+        size_result = await col.aggregate(size_pipeline).to_list(length=1)
+        sec_size_result = await sec_col.aggregate(size_pipeline).to_list(length=1)
         total_size = (size_result[0]['total'] if size_result else 0) + (sec_size_result[0]['total'] if sec_size_result else 0)
 
         # Downloads today
@@ -211,7 +176,12 @@ async def handle_stats(client, message):
         await msg.edit_text("<b>Error fetching stats. Please try again later.</b>")
 
 async def handle_stop_button(bot, message):
-    msg = await bot.send_message(text="**🔄 𝙿𝚁𝙾𝙲𝙴𝚂𝚂𝙴𝚂 𝚂𝚃𝙾𝙿𝙴𝙳. 𝙱𝙾𝚃 𝙸𝚂 𝚁𝙴𝚂𝚃𝙰𝚁��𝙶...**", chat_id=message.chat.id)       
+    msg = await bot.send_message(
+        text="**\U0001f504 Process stopped. Bot is restarting...**",
+        chat_id=message.chat.id,
+    )
     await asyncio.sleep(3)
-    await msg.edit("**✅️ 𝙱𝙾𝚃 𝙸𝚂 𝚁𝙴𝚂𝚃𝙰�𝙴𝙳. 𝙽𝙾𝚆 𝚈𝙾𝚄 𝙲𝙰𝙽 𝚄𝚂𝙴 𝙼𝙴**")
+    await msg.edit(
+        "**\u2705 Bot restarted. You can use it again.**"
+    )
     os.execl(sys.executable, sys.executable, *sys.argv)
