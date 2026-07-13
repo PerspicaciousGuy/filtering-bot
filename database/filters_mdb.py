3b8cb47c79e884d21d820b2805534e033021faed
PerspicaciousGuy <joy.0839b@gmail.com>
2026-07-13 07:03:18 +0530

    refactor: migrate database operations to use AsyncIOMotorClient for improved performance

--- database/filters_mdb.py
+++ database/filters_mdb.py
@@ -1,22 +1,22 @@
-
-
-import pymongo
-from pymongo.errors import PyMongoError
-from info import OTHER_DB_URI, DATABASE_NAME
 import logging
+
+from motor.motor_asyncio import AsyncIOMotorClient
+from pymongo.errors import PyMongoError
+
+from database.filter_stats import build_filter_stats
+from info import DATABASE_NAME, OTHER_DB_URI
+
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.ERROR)
 
-myclient = pymongo.MongoClient(OTHER_DB_URI)
-mydb = myclient[DATABASE_NAME]
-
+client = AsyncIOMotorClient(OTHER_DB_URI)
+database = client[DATABASE_NAME]
 
 
 async def find_filter(group_id, name):
-    mycol = mydb[str(group_id)]
-
+    collection = database[str(group_id)]
     try:
-        file = mycol.find_one({"text": name})
+        file = await collection.find_one({"text": name})
         if file is None:
             return None, None, None, None
         return file["reply"], file["btn"], file.get("alert"), file["file"]
@@ -26,46 +26,28 @@ async def find_filter(group_id, name):
 
 
 async def get_filters(group_id):
-    mycol = mydb[str(group_id)]
-
+    collection = database[str(group_id)]
     texts = []
-    query = mycol.find()
     try:
-        for file in query:
-            text = file['text']
-            texts.append(text)
+        async for file in collection.find():
+            texts.append(file["text"])
     except (KeyError, PyMongoError):
         logger.exception("Failed to list filters")
     return texts
 
 
 async def del_all(message, group_id, title):
-    if str(group_id) not in mydb.list_collection_names():
+    if str(group_id) not in await database.list_collection_names():
         await message.edit_text(f"Nothing to remove in {title}!")
         return
 
-    mycol = mydb[str(group_id)]
+    collection = database[str(group_id)]
     try:
-        mycol.drop()
+        await collection.drop()
         await message.edit_text(f"All filters from {title} has been removed")
     except PyMongoError:
         logger.exception("Failed to remove all filters")
         await message.edit_text("Couldn't remove all filters from group!")
-        return
 
 
-async def filter_stats():
-    collections = mydb.list_collection_names()
-
-    if "CONNECTION" in collections:
-        collections.remove("CONNECTION")
-
-    totalcount = 0
-    for collection in collections:
-        mycol = mydb[collection]
-        count = mycol.count()
-        totalcount += count
-
-    totalcollections = len(collections)
-
-    return totalcollections, totalcount
+filter_stats = build_filter_stats(database)
