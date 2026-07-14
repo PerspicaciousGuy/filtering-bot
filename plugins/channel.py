b3ede0eb23d4685ebd3e26b7f7409af0536d8cde
PerspicaciousGuy <joy.0839b@gmail.com>
2026-07-14 19:18:23 +0530

    feat: refactor channel and file deletion handlers to utilize configured channels

--- plugins/channel.py
+++ plugins/channel.py
@@ -1,12 +1,13 @@
 
 
 from pyrogram import Client, filters
-from info import CHANNELS
+
 from database.ia_filterdb import save_file
+from EbookGuy.shared.configured_channels import configured_channel_filter
 
 media_filter = filters.document | filters.video | filters.audio
 
-@Client.on_message(filters.chat(CHANNELS) & media_filter)
+@Client.on_message(media_filter & configured_channel_filter("file_channel_ids"))
 async def media(bot, message):
     media = getattr(message, message.media.value, None)
     media.caption = message.caption
