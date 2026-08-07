ce0701cb163f2bfddbc327181b0bca04cb28ea3d
PerspicaciousGuy <joy.0839b@gmail.com>
2026-08-07 15:09:40 +0530

    Refactor: reorder import statements for improved clarity and organization

--- plugins/commands_downloads.py
+++ plugins/commands_downloads.py
@@ -1,8 +1,6 @@
 from EbookGuy.features.downloads.callbacks import handle_download_book_callback
-from EbookGuy.features.downloads.limits import (
-    check_and_increment_download,
-    send_auto_delete_message,
-)
+from EbookGuy.features.downloads.auto_delete import send_auto_delete_message
+from EbookGuy.features.downloads.limits import check_and_increment_download
 from EbookGuy.features.downloads.start import get_start_buttons, handle_start
 
 __all__ = [
